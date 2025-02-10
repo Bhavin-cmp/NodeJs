d2e086d6af0bcda83d0d4d68ba13b3263cf04977
Bhavin <patelbc94@gmail.com>
2025-02-11 00:10:23 +0530

    Performin crud operation on file

--- server.js
+++ server.js
@@ -5,20 +5,29 @@ import { fileURLToPath } from "url";
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 
-const dirPath = path.join(__dirName, "Files");
+const dirPath = path.join(__dirName, "crud");
+const filePath = `${dirPath}/test.txt`;
 
-//!Create Multiple File
-for (let i = 0; i < 5; i++) {
-  fs.writeFileSync(
-    `${dirPath}/Hello${i}.txt`,
-    `Hello There Bhavin Patel This is File Number ${i}`
-  );
-}
+//! Creating File
+fs.writeFileSync(filePath, "Hello Bhavin Patel");
 
-//!Getting List of available File
-/* fs.readdir(dirPath, (err, files) => {
-  files.forEach((file) => {
-    console.log("Files List", file);
-  });
+//!  Read File
+fs.readFile(filePath, "utf-8", (err, item) => {
+  console.log("first", item);
 });
- */
+
+//!Update File
+
+/* fs.appendFile(filePath, "I am a full stack developer", (err) => {
+  if(!err) console.log(" File is updated");
+}); */
+
+//! Reanem file
+
+/* fs.rename(filePath, `${dirPath}/Hello.txt`, (err) => {
+  if (!err) console.log(" File is Name is Changed");
+}); */
+
+//! Delete File
+
+fs.unlinkSync(`${dirPath}/test.txt`);
